888eaa00bff47d4dc63ebfe1e814e3092182f4c6
Anthony Heitzeberg <a.heitzeberg007@gmail.com>
2026-05-24 22:33:46 -0500

    Update the properties for the app and create the Block.java

---
+++ src/main/java/com/supplychain/blockchain_tracker/blockchain/Block.java
@@ -0,0 +1,43 @@
+package com.supplychain.blockchain_tracker.blockchain;
+
+import lombok.Getter;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+
+@Getter
+public class Block {
+
+    private final int index;
+    private final String timestamp;
+    private final String data;
+    private final String previousHash;
+    private final String hash;
+
+
+    public Block(int index, String data, String previousHash) {
+        this.index = index;
+        this.timestamp = Instant.now().toString();
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = calculateHash();
+    }
+
+    private String calculateHash() {
+        String input = index + timestamp + data + previousHash;
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = messageDigest.digest(input.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+            for (byte b: hashBytes) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-256 alogirhtm was not found!", e);
+        }
+    }
+}
