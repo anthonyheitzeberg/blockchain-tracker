fde9798b0c3d9666208247d496f4bf34f9e0aa1d
Anthony Heitzeberg <a.heitzeberg007@gmail.com>
2026-05-24 21:50:55 -0500

    Initial commit

---
+++ src/main/java/com/supplychain/blockchain_tracker/BlockchainTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.supplychain.blockchain_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supplychain/blockchain_tracker/BlockchainTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplychain.blockchain_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
